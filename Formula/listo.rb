773efe5d31d2be07534a684622057a61c82c62d3
Yuto Tagashira <ut0s.dev@gmail.com>
2026-06-12 14:39:02 +0900

    listo v0.2.4

--- Formula/listo.rb
+++ Formula/listo.rb
@@ -1,9 +1,9 @@
 class Listo < Formula
   desc "Local todo.txt web UI, REST API, and MCP server"
   homepage "https://github.com/ut0s/homebrew-listo"
-  url "https://github.com/ut0s/homebrew-listo/releases/download/v0.2.3/listo-v0.2.3.tar.gz"
-  version "0.2.3"
-  sha256 "573e3d87374b2ca00fa0725f3c3ed2141fc21d5c7ff72c077ebbd8dabb435def"
+  url "https://github.com/ut0s/homebrew-listo/releases/download/v0.2.4/listo-v0.2.4.tar.gz"
+  version "0.2.4"
+  sha256 "4213fb09910490f4764aa02e64778d7c3367dd2ae5a84db88d276bc68a672d79"
   license :cannot_represent
 
   depends_on "node"
